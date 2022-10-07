94332c8e516a0f686e093e49a91c971c42c49af4
Samba Diaw <sambachimerediaw@gmail.com>
2022-10-08 01:49:48 +0200

    improve ui && config firebase

--- src/components/directory/directory.styles.jsx
+++ src/components/directory/directory.styles.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const DirectoryContainer = styled.div`
   width: 100%;
+  /* height: 100vh; */
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
